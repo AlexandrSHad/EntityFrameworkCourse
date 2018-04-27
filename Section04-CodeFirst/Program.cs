7697ea833b264b8deecd16a744a6b581c976af8b
Alexandr SHad <shadurskij@gmail.com>
2018-04-27 23:38:05 +0300

    group join

--- Section04-CodeFirst/Program.cs
+++ Section04-CodeFirst/Program.cs
@@ -10,6 +10,21 @@ namespace Section04_CodeFirst
     {
         static void Main(string[] args)
         {
+            var context = new VidzyDbContext();
+
+            var genres = context.Genres
+                .GroupJoin(context.Videos,
+                    g => g.Id, v => v.GenreId,
+                    (g, v) => new {
+                        GenreName = g.Name,
+                        VideosCount = v.Count()
+                    })
+                .OrderByDescending(r => r.VideosCount);
+
+            foreach (var g in genres)
+            {
+                Console.WriteLine("{0} ({1})", g.GenreName, g.VideosCount);
+            }
         }
     }
 }
