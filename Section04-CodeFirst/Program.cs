feac2d4720f03eb042f12ae961c65b62832170d7
Alexandr SHad <shadurskij@gmail.com>
2018-04-28 14:01:06 +0300

    section 7 - lazy loading, eager loading, explicit loading

--- Section04-CodeFirst/Program.cs
+++ Section04-CodeFirst/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,44 @@ namespace Section04_CodeFirst
             foreach (var g in genres)
             {
                 Console.WriteLine("{0} ({1})", g.GenreName, g.VideosCount);
+            }
+
+
+            //// To enable lazy loading, you need to declare navigation properties
+            //// as virtual. Look at the Video class.
+
+            //var videos = context.Videos.ToList();
+
+            //Console.WriteLine();
+            //Console.WriteLine("LAZY LOADING");
+            //foreach (var v in videos)
+            //    Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
+
+            //// Eager loading
+            //var videosWithGenres = context.Videos.Include(v => v.Genre).ToList();
+
+            //Console.WriteLine();
+            //Console.WriteLine("EAGER LOADING");
+            //foreach (var v in videosWithGenres)
+            //    Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
+
+            // Explicit loading
+
+            // NOTE: At this point, genres are already loaded into the context,
+            // so the following line is not going to make a difference. If you 
+            // want to see expicit loading in action, comment out the eager loading 
+            // part as well as the foreach block in the lazy loading.
+
+            // Explicit loading
+            var videos = context.Videos.ToList();
+
+            var genresIds = videos.Select(v => v.GenreId).Distinct();
+
+            context.Genres.Where(g => genresIds.Contains(g.Id)).Load();
+
+            foreach (var v in videos)
+            {
+                Console.WriteLine("{0} [{1}]", v.Name, v.Genre.Name);
             }
         }
     }
